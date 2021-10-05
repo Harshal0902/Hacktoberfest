0e662cada4793b123e628faaf193b8618cf54154
Adam Wojnicki <wojnicki.ada1@gmial.com>
2021-10-05 21:40:17 +0200

    todo list mapping

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>APP PLACEHOLDER</div>;
+  const [todos, setTodos] = useState([
+    { id: 1, text: "todo 1", completed: true },
+    { id: 2, text: "todo 2", completed: false },
+    { id: 3, text: "todo 3", completed: true },
+  ]);
+  return (
+    <div>
+      {todos.map((todo) => (
+        <p key={todo.id}>
+          {todo.text} <input type="checkbox" checked={todo.completed} />
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default App;
